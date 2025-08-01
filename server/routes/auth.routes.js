b66bf90156fb15708396489f05a17d56bd6b9031
Abhay-333 <abhayd0331@gmail.com>
2025-08-01 10:07:11 +0530

    update

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -11,10 +11,9 @@ import { checkAuth } from "../middlewares/checkAuth.js";
 const authRouter = express(Router());
 
 authRouter.get("/", Home);
-authRouter.post("/signup", signUp);
-authRouter.post("/signup", signUp);
-authRouter.post("/login", login);
-authRouter.post("/logout", logout);
-authRouter.get("/getUser", checkAuth, getUserData);
+authRouter.post("signup", signUp);
+authRouter.post("login", login);
+authRouter.post("logout", logout);
+authRouter.get("getUser", checkAuth, getUserData);
 
 export default authRouter;
